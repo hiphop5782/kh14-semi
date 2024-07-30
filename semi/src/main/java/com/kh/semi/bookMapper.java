718862cba073fb3ea866bd9d33bf6688f3fa0ca2
parkjunho2 <pp01024689860@gmail.com>
2024-07-30 17:23:08 +0900

    upload

---
+++ semi/src/main/java/com/kh/semi/bookMapper.java
@@ -0,0 +1,16 @@
+package com.kh.semi;
+
+import javax.swing.tree.RowMapper;
+import javax.swing.tree.TreePath;
+
+import org.springframework.stereotype.Service;
+@Service
+public class bookMapper implements RowMapper {
+
+	@Override
+	public int[] getRowsForPaths(TreePath[] path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
