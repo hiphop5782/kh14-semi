7509078cccef151ff009c174f4662c9e7e8fc9e7
wjddpfla1022 <wjddpfla98@naver.com>
2024-08-06 17:40:24 +0900

    포켓몬 컨트롤러 이미지제공페이지 코드

--- semi/src/main/java/com/kh/semi/controller/PoketmonController.java
+++ semi/src/main/java/com/kh/semi/controller/PoketmonController.java
@@ -2,10 +2,21 @@ package com.kh.semi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/poketmon")
 public class PoketmonController {
-	
+	//이미지 제공 페이지
+//		@RequestMapping("/image")
+//		public String image(@RequestParam int poketmonNo) {
+//			try {//플랜A-이미지가 있으면
+//			Integer attachmentNo=poketmonDao.findImage(poketmonNo);
+//			return "redirect:/attach/download?attachmentNo="+attachmentNo;
+//			}
+//			catch(Exception e) {//플랜B-이미지가 없으면
+//				return "redirect:https://placehold.co/150";
+//			}
+//		}
 	
 }
