62f72465e45278bf6b89f81c2c12e72033a9e581
parkhewoo <pkhwoo91@gmail.com>
2024-08-06 17:49:58 +0900

    abc

---
+++ semi/src/main/java/com/kh/semi/MemberDto.java
@@ -0,0 +1,11 @@
+package com.kh.semi;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+	private String memberId;
+	private String memberPw;
+	private int memberNo;
+	private String memberNick;
+}
