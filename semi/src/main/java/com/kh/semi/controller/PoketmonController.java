45cc2f2636e5bfdfb84663b1f992b59d503379cc
dmstn12 <supreme741@naver.com>
2024-08-06 20:34:52 +0900

    커밋확인

--- semi/src/main/java/com/kh/semi/controller/PoketmonController.java
+++ semi/src/main/java/com/kh/semi/controller/PoketmonController.java
@@ -19,93 +19,8 @@
 //@RequestMapping("/poketmon")
 //public class PoketmonController {
 //
-package com.kh.semi.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
-////	@RequestMapping("/insert2")//처리
-//	@PostMapping("/insert")//POST방식만 처리
-//	public String insert(@ModelAttribute PoketmonDto dto) {
-//		dao.insert(dto);
-//		return "redirect:insertComplete"; //리다이렉트(상대경로)
-//	}
-//	
-//	@GetMapping("/insert") // GET방식만 처리
-//	public String insert() {
-//		return "/WEB-INF/views/poketmon/insert1.jsp";
-//	}
-//
-//////	@RequestMapping("/insert2")//처리
-////	@PostMapping("/insert")//POST방식만 처리
-////	public String insert(@ModelAttribute PoketmonDto dto) {
-////		dao.insert(dto);
-////		return "redirect:insertComplete"; //리다이렉트(상대경로)
-////	}
-////	
-//	// 이 메소드는 하나의 트랜잭션(세트메뉴)으로 취급됨
-//	// Database의 C.U.D 작업을 여러 번 하는 경우 전체를 하나로 생각하여 처리
-//	// 다 되거나, 다 안되거나 둘 중 하나밖에 없음
-//	@Transactional
-//	@PostMapping("/insert") // POST방식만 처리
-//	public String insert(@ModelAttribute PoketmonDto poketmonDto, @RequestParam MultipartFile attach)
-//			throws IllegalStateException, IOException {
-//		// 1. 포켓몬스터 번호 생성
-//		int poketmonNo = poketmonDao.sequence();
-//		poketmonDto.setPoketmonNo(poketmonNo);
-//
-//		// 2. 포켓몬스터 등록
-//		poketmonDao.insertWithSequence(poketmonDto);
-//
-//		// MultipartFile은 비어있을 수 있으므로 조건으로 검사를 해야한다.
-//		if (!attach.isEmpty()) {
-//			int attachmentNo = attachmentService.save(attach);
-//			poketmonDao.connect(poketmonNo, attachmentNo);
-//
-	// 이 메소드는 하나의 트랜잭션(세트메뉴)으로 취급됨
-	// Database의 C.U.D 작업을 여러 번 하는 경우 전체를 하나로 생각하여 처리
-	// 다 되거나, 다 안되거나 둘 중 하나밖에 없음
-//	@Transactional
-////	@PostMapping("/insert") // POST방식만 처리
-//	public String insert(@ModelAttribute PoketmonDto poketmonDto, @RequestParam MultipartFile attach)
-//			throws IllegalStateException, IOException {
-//		// 1. 포켓몬스터 번호 생성
-//		int poketmonNo = poketmonDao.sequence();
-//		poketmonDto.setPoketmonNo(poketmonNo);
-//
-//		// 2. 포켓몬스터 등록
-//		poketmonDao.insertWithSequence(poketmonDto);
-//
-//		// MultipartFile은 비어있을 수 있으므로 조건으로 검사를 해야한다.
-//		if (!attach.isEmpty()) {
-//			int attachmentNo = attachmentService.save(attach);
-//			poketmonDao.connect(poketmonNo, attachmentNo);
-//
-	
-
-	@RequestMapping("/insertComplete") // 완료
-	public String insertComplete() {
-		return "/WEB-INF/views/poketmon/insert3.jsp";
-	}
-
-	@RequestMapping("/detail")
-	public String detail() {
-		return "ㅎㅇ";
-//		PoketmonDto dto = poketmonDao.selectOne(poketmonNo);
-//		model.addAttribute("dto", dto);
-//		return "/WEB-INF/views/poketmon/detail.jsp";
-	}
-	
-	@RequestMapping("/delete")
-	public String delete() {
-		return"hello";
-	}
 
 
 	//이미지 제공 페이지
@@ -174,4 +89,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 	
 
 
-}
+
