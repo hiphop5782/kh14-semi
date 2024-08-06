cd5d15af5d7bcc09c5c850cbc2d5a2d490abb17d
gykim94 <dgykim94@naver.com>
2024-08-06 17:40:16 +0900

    MemberDto 생성1

---
+++ semi/src/main/java/com/kh/semi/MemberDto.java
@@ -0,0 +1,10 @@
+package com.kh.semi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
+@Data
+public class MemberDto {
+	private String test;
+	private int test02;
+}
