aa3de91fbee9aa07f3c9589cd7276986d1a3eb51
shwjdgus1234 <shwjdgus12345@naver.com>
2024-08-06 17:40:45 +1000

    포켓몬 디테일 컨트롤러

--- semi/src/main/java/com/kh/semi/controller/PoketmonController.java
+++ semi/src/main/java/com/kh/semi/controller/PoketmonController.java
@@ -1,11 +1,21 @@
 package com.kh.semi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/poketmon")
 public class PoketmonController {
 	
+	@RequestMapping("/detail")
+	public String detail() {
+		return "ㅎㅇ";
+//		PoketmonDto dto = poketmonDao.selectOne(poketmonNo);
+//		model.addAttribute("dto", dto);
+//		return "/WEB-INF/views/poketmon/detail.jsp";
+	}
 	
 }
